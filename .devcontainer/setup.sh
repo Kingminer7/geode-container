6bbfe37153140e4ea8e3b2f97a757e3bbcbcf3e6
km7 dev <100500023+Kingminer7@users.noreply.github.com>
2024-10-04 17:11:57 +0000

    use geode sdk install-linux

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -12,28 +12,30 @@ sudo apt install -y git clang-17 clang-tools-17 lld-17
 pip install cmake --upgrade # because for whatever reason it installs an outdated version, ig source repos arent up to date!
 rm llvm.sh
 
-wget https://github.com/geode-sdk/cli/releases/download/v3.2.0/geode-cli-v3.2.0-linux.zip
-unzip geode-cli-v3.2.0-linux.zip
+wget https://github.com/geode-sdk/cli/releases/download/v3.2.1/geode-cli-v3.2.1-linux.zip
+unzip geode-cli-v3.2.1-linux.zip
 chmod +x geode
 sudo mv geode /usr/local/bin/
-rm geode-cli-v3.2.0-linux.zip
+rm geode-cli-v3.2.1-linux.zip
 
-wget https://github.com/Jake-Shadle/xwin/releases/download/0.6.5/xwin-0.6.5-x86_64-unknown-linux-musl.tar.gz
-tar -xzf xwin-0.6.5-x86_64-unknown-linux-musl.tar.gz
-chmod +x xwin-0.6.5-x86_64-unknown-linux-musl/xwin
-sudo mv xwin-0.6.5-x86_64-unknown-linux-musl/xwin /usr/local/bin/
-rm -rf xwin-0.6.5-x86_64-unknown-linux-musl
-rm xwin-0.6.5-x86_64-unknown-linux-musl.tar.gz
+# wget https://github.com/Jake-Shadle/xwin/releases/download/0.6.5/xwin-0.6.5-x86_64-unknown-linux-musl.tar.gz
+# tar -xzf xwin-0.6.5-x86_64-unknown-linux-musl.tar.gz
+# chmod +x xwin-0.6.5-x86_64-unknown-linux-musl/xwin
+# sudo mv xwin-0.6.5-x86_64-unknown-linux-musl/xwin /usr/local/bin/
+# rm -rf xwin-0.6.5-x86_64-unknown-linux-musl
+# rm xwin-0.6.5-x86_64-unknown-linux-musl.tar.gz
 
-mkdir -p ~/xwin/splat
-cd ~/xwin
-xwin --arch x86_64 --accept-license splat --include-debug-libs --output splat
+# mkdir -p ~/xwin/splat
+# cd ~/xwin
+# xwin --arch x86_64 --accept-license splat --include-debug-libs --output splat
 
 git clone https://github.com/matcool/clang-msvc-sdk.git toolchain
 
 geode sdk install ~/geode
 export GEODE_SDK=~/geode
 geode sdk install-binaries
+
+geode sdk install-linux
 
 sudo apt-get clean
 sudo rm -rf /var/lib/apt/lists/*
